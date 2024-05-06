61e624f92204e255f79786e7d8b15a6118121d46
abdums <ace272@icloud.com>
2024-05-05 22:06:19 -0400

    Straight line wins fixed

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def next_turn(row, column):
                 label.config(text=(players[0]+" wins"))
 
             elif check_winner() == "Tie":
-                label.config(text=("Tie!"))
+                label.config(text="Tie!")
 
         else:
 
@@ -32,25 +32,41 @@ def next_turn(row, column):
                 label.config(text=(players[1]+" wins"))
 
             elif check_winner() == "Tie":
-                label.config(text=("Tie!"))
-                
+                label.config(text="Tie!")
+
 def check_winner():
     
     for row in range(3):
         if buttons[row][0]['text'] == buttons[row][1]['text'] == buttons[row][2]['text'] !="":
+            buttons[row][0].config(bg = "purple")
+            buttons[row][1].config(bg = "purple")
+            buttons[row][2].config(bg = "purple")
             return True
 
     for column in range(3):
-        if buttons[column][0]['text'] == buttons[column][1]['text'] == buttons[column][2]['text'] !="":
+        if buttons[0][column]['text'] == buttons[1][column]['text'] == buttons[2][column]['text'] != "":
+            buttons[0][column].config(bg="purple")
+            buttons[1][column].config(bg="purple")
+            buttons[2][column].config(bg="purple")
             return True
         
     if buttons[0][0]['text'] == buttons[1][1]['text'] == buttons [2][2]['text'] != "":
+        buttons[0][0].config(bg = "purple")
+        buttons[1][1].config(bg = "purple")
+        buttons[2][2].config(bg = "purple")
         return True
         
     elif buttons[0][2]['text'] == buttons[1][1]['text'] == buttons [2][0]['text'] != "":
+        buttons[0][2].config(bg = "purple")
+        buttons[1][1].config(bg = "purple")
+        buttons[2][0].config(bg = "purple")
         return True
     
     elif empty_spaces() is False:
+        
+        for row in range(3):
+            for column in range(3):
+                buttons[row][column].config(bg = "green")
         return "Tie"
 
     else:
@@ -64,8 +80,8 @@ def empty_spaces():
         for column in range(3):
             if buttons[row][column]['text'] != "":
                 spaces -= 1
-                
-    if spaces ==0:
+
+    if spaces == 0:
         return False
     else:
         return True
