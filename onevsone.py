197593eb93d2fc5eb6affbc07560909f10bc162f
abdums <ace272@icloud.com>
2024-05-16 05:09:01 -0400

    Minor adjustments on onevsone.py
    Simplified turn switch logic amongst other stuff

--- onevsone.py
+++ onevsone.py
@@ -16,24 +16,14 @@ def next_turn(row, column):
     global player
 
     if buttons[row][column]['text'] == "" and check_winner() is False:
-        if player == players[0]:
-            buttons[row][column]['text'] = player
-            if check_winner() is False:
-                player = players[1]
-                label.config(text=(players[1] + " turn"))
-            elif check_winner() is True:
-                label.config(text=(players[0] + " wins"))
-            elif check_winner() == "Tie":
-                label.config(text="Tie!")
-        else:
-            buttons[row][column]['text'] = player
-            if check_winner() is False:
-                player = players[0]
-                label.config(text=(players[0] + " turn"))
-            elif check_winner() is True:
-                label.config(text=(players[1] + " wins"))
-            elif check_winner() == "Tie":
-                label.config(text="Tie!")
+        buttons[row][column]['text'] = player
+        if check_winner() is False:
+            player = players[1] if player == players[0] else players[0]
+            label.config(text=(player + " turn"))
+        elif check_winner() is True:
+            label.config(text=(player + " wins!"))
+        elif check_winner() == "Tie":
+            label.config(text="Tie!")
 
 def check_winner():
     for row in range(3):
@@ -74,10 +64,7 @@ def empty_spaces():
         for column in range(3):
             if buttons[row][column]['text'] != "":
                 spaces -= 1
-    if spaces == 0:
-        return False
-    else:
-        return True
+    return spaces != 0
 
 def new_game():
     global player
@@ -94,7 +81,7 @@ def main():
     window.title("Tic-Tac-Toe")
 
     # Creating game widgets
-    label = Label(text=player + "turn", font=('consolas', 40))
+    label = Label(text=player + " turn", font=('consolas', 40))
     label.pack(side="top")
 
     reset_button = Button(text="restart", font=('consolas', 20), command=new_game)
